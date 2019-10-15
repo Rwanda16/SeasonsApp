b601f751d326163824f1b229d23272708eee503a
Rwanda16 <ghabiman@gmail.com>
2019-10-15 15:56:47 -0400

    Handling errors within the App

--- src/index.js
+++ src/index.js
@@ -5,19 +5,27 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: null };
+    this.state = { lat: null, errMessage: "" };
     window.navigator.geolocation.getCurrentPosition(
       position => {
         //Always call setState to update state object
         this.setState({ lat: position.coords.latitude });
       },
-      err => console.log(err)
+      err => {
+        this.setState({ errMessage: err.message });
+      }
     );
   }
 
   //**We have to always define render but not the constructor**
   render() {
-    return <div>Latitude:{this.state.lat}</div>;
+    return (
+      <div>
+        Latitude:{this.state.lat}
+        <br />
+        Error: {this.state.errMessage}
+      </div>
+    );
   }
 }
 
